089bbee298697a379150bc57753a757ace41fb96
Pannu786 <thepannu786@gmail.com>
2022-05-14 22:11:05 +0530

    _app.js added layout cmp + layout cmp added children + base navbar setup

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,22 @@
 import React from 'react';
-
 import Head from 'next/head';
 
-const Layout = () => {
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+const Layout = ({ children }) => {
   return (
     <div className='layout'>
       <Head>
         <title>De Pannu XoXo</title>
       </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className='main-container'>{children}</main>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 };
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
+import Link from 'next/link';
+import { AiOutlineShopping } from 'react-icons/ai';
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className='navbar-container'>
+      <p>
+        <Link href='/' className='logo'>
+          Pannu HeadPhones
+        </Link>
+      </p>
+      <button type='button' className='cart-icon' onClick=''>
+        <AiOutlineShopping />
+        <span className='cart-item-qty'>{2}</span>
+      </button>
+    </div>
+  );
 };
 
 export default Navbar;
