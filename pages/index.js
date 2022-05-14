ad1e3177465e4c8ec51338037c3ba5933fbfd863
Pannu786 <thepannu786@gmail.com>
2022-05-14 16:58:58 +0530

    created env file + connect sanity with next + added product at HeroBanner cmp

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,35 @@
 import React from 'react';
 
+import { client } from '../lib/client';
 import { Product, FooterBanner, HeroBanner } from '../components';
 
-const Home = () => {
+const Home = ({ products, bannerData }) => {
   return (
     <>
-      <HeroBanner />
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+      {console.log(bannerData)}
       <div className='products-heading'>
         <h2>Best of Best Products here</h2>
         <p>Speaker are boom here mate</p>
       </div>
       <div className='products-container'>
-        {['Product1', 'Product2'].map((pro) => pro)}
+        {products?.map((pro) => pro.name)}
       </div>
       <FooterBanner />
     </>
   );
+};
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
 };
 
 export default Home;
