fd0416e6a88aa3cae076684453472de5f47c0764
Pannu786 <thepannu786@gmail.com>
2022-05-19 22:47:44 +0530

    fixed the add cart item error

--- components/Product.js
+++ components/Product.js
@@ -1,26 +1,25 @@
 import React from 'react';
-
 import Link from 'next/link';
+
 import { urlFor } from '../lib/client';
 
 const Product = ({ product: { image, name, slug, price } }) => {
   return (
     <div>
       <Link href={`/product/${slug.current}`}>
-        <div className='product-card'>
-          <img
+        <div className="product-card">
+          <img 
             src={urlFor(image && image[0])}
             width={250}
             height={250}
-            className='product-image'
-            alt=''
+            className="product-image"
           />
-          <p className='product-name'>{name}</p>
-          <p className='product-price'>${price}</p>
+          <p className="product-name">{name}</p>
+          <p className="product-price">${price}</p>
         </div>
       </Link>
     </div>
-  );
-};
+  )
+}
 
-export default Product;
+export default Product
